485e5cba26d88330fb67a2dbbc179c34f2595810
Niccolò Piazzesi <npiazzesi@gmail.com>
2020-12-21 11:57:39 +0100

    need to add multithread then python done

--- Assignment2/Python/benchmark.py
+++ Assignment2/Python/benchmark.py
@@ -1,16 +1,58 @@
 import threading
 from functools import wraps
+import time
+import csv
+
+
+
+def _timeFunctionCalls(rounds,function,*args,**kwargs):
+    results = dict()
+    for i in range(rounds):
+        run = 0
+        time1 = time.perf_counter()
+        function(*args, **kwargs)
+        run = time.perf_counter() - time1
+        results[i+1] = run
+    return results
 
 def benchmark(warmups=0, iter=1, verbose=False, csv_file=None):
     def decorator(func):
         @wraps(func)
         def wrapper(*args,**kwargs):
-            for i in range(warmups):
-                if verbose:
-                    print(" warmup round n.{}: ".format(i) )
-                print(func(*args,**kwargs))
+            warm = dict()
+            invoke = _timeFunctionCalls(iter,func,*args,**kwargs)
+            if verbose:
+                if warmups==0:
+                    print("---skipping warmups---")
+                else:
+                    print("WARMUP ROUNDS\n")
+                    warm = _timeFunctionCalls(warmups,func,*args,**kwargs)
+                    print("{:<8} {:<15}".format('round', 'time'))
+                    for k,v in warm.items():
+                        print("{:<8} {:<15}".format(k,v))
+                
+                print("\nINVOCATION ROUNDS")  
+                print("{:<8} {:<15}".format('round', 'time'))
+                for k,v in invoke.items():
+                    print("{:<8} {:<15}".format(k,v))
             
+            avg = sum(invoke.values()) / iter
+            variance = sum((v - avg) ** 2 for v in invoke.values()) / iter
+            print("{:<8} {:<15}".format('average time',avg))
+            print("{:<8} {:<15}".format('variance', variance))
             
+            if csv_file:
+                with open(csv_file, "w") as f:
+                    writer = csv.writer(f)      
+                    writer.writerow(['run num', 'is warmup', 'timing'])
+                    for k,v in warm.items():
+                        writer.writerow([k,'yes',v])
+                    for k,v in invoke.items():
+                        writer.writerow([k+warmups,'no',v])
+                    
+                    writer.writerow(['average', avg])
+                    writer.writerow(['variance',variance])
+                    
             return func
         return wrapper
     return decorator
@@ -26,9 +68,9 @@ def fibonacci(n=0):
 
 
 def test():
-    fib = benchmark(warmups=2,verbose=True)
+    fib = benchmark(warmups=3,iter=3,verbose=True,csv_file='file.csv')
     fib = fib(fibonacci)
-    print(fib(10))
+    fib(5)
    
 
 test()
