3baed63d8cc84ee567725370a2d1c5c402f61b1a
Niccolò Piazzesi <npiazzesi@gmail.com>
2020-12-26 12:48:14 +0100

    started mapreduce

--- Assignment2/Java/MapReduce-Piazzesi/src/main/java/assignment/framework/mapreduce/piazzesi/MapReduce.java
+++ Assignment2/Java/MapReduce-Piazzesi/src/main/java/assignment/framework/mapreduce/piazzesi/MapReduce.java
@@ -5,10 +5,26 @@
  */
 package assignment.framework.mapreduce.piazzesi;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 /**
  *
  * @author nicco
  */
-public class MapReduce {
+public abstract class MapReduce {
     
+    
+    public final <T,S,K,V> void compute(Path p, File f){
+        Stream<Pair<K,V>> i = read(p);
+        Stream<Pair<T,S>> out = map(i);
+        write(f,reduce(out.sorted(compare())));
+    }
+    protected abstract <K,V> Stream<Pair<K,V>> read(Path p);
+    protected abstract <T,S,K,V> Stream<Pair<T, S>> map(Stream<Pair<K,V>> in);
+    protected abstract <T> Comparator<T> compare();
+    protected abstract <T,S,K,V> Stream<Pair<T, S>> reduce(Stream<Pair<K,V>> in);
+    protected abstract <K,V> void write(File f, Stream<Pair<K,V>> r );
 }
