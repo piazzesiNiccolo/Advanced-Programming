2572b2bcc36f83b487e3e3f8354c8157fe279c40
Niccolò Piazzesi <npiazzesi@gmail.com>
2020-12-31 13:07:21 +0100

    m

--- Assignment2/Python/benchmark.py
+++ Assignment2/Python/benchmark.py
@@ -45,8 +45,6 @@ def benchmark(warmups=0, iter=1, verbose=False, csv_file=None):
             if csv_file:
                 with open(csv_file, "a") as f:
                     writer = csv.writer(f)
-                    
-                    writer.writerow([threading.current_thread().getName(),'',''])
                    
                     writer.writerow(['run num', 'is warmup', 'timing'])
                     for k, v in warm.items():
@@ -55,8 +53,7 @@ def benchmark(warmups=0, iter=1, verbose=False, csv_file=None):
                         writer.writerow([k+warmups, 'no', v])
                         
                     
-                    writer.writerow(['average', avg])
-                    writer.writerow(['variance', variance])
+                   
                    
             return result
         return wrapper
